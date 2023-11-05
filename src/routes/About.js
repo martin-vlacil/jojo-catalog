ec831296c477f44792e04d883dd572035a4bc01c
Martin Vlacil <martinvlacil.mv@gmail.com>
2023-11-05 18:30:19 +0100

    Cleaning and adding lorem ipsum

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,3 +1,19 @@
 export default function About() {
-    return <h1>About</h1>
+    return (
+        <div className = "about">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in leo ultrices, ullamcorper elit in, tristique arcu. Fusce in finibus libero. Sed ac quam eu purus tristique venenatis. Vestibulum nec fringilla lectus, vel bibendum lacus. Quisque nec vulputate nisl. Suspendisse tincidunt lectus vel lectus lacinia, nec volutpat orci facilisis. Aenean sed justo in leo dignissim vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+            Pikachu ipsum voltorb jigglypuff psyduck poliwag caterpie charmander. Ivysaur bulbasaur psyduck poliwrath jigglypuff jolteon charizard squirtle. Bulbasaur tangela tentacool machop dugtrio jigglypuff metapod tangela. Nidoran vileplume vulpix horsea dratini hitmonlee vileplume zubat. Cubone charmander vulpix dewgong charmander mew metapod meowth. Bulbasaur charmeleon wartortle haunter poliwag spearow bellsprout kakuna. Pidgeot zubat jigglypuff oddish nidoran sandshrew machop poliwag.
+            
+            Squirtle meowth vulpix charmander clefable machop cloyster. Bellsprout pidgeotto sandslash jigglypuff machop rattata venomoth. Rattata pidgey poliwhirl mankey venomoth kabuto tangela vileplume. Mewtwo mankey raticate nidorino blastoise sandslash. Grimer machop kabuto venusaur electrode jolteon caterpie mewtwo. Jolteon poliwag farfetchd paras meowth fearow. Golbat bulbasaur machop jigglypuff dugtrio metapod oddish.
+            
+            Eevee charmeleon dugtrio dragonair jigglypuff dragonite butterfree. Jigglypuff dragonair mankey clefable meowth raticate charmeleon dugtrio. Nidoran jigglypuff hitmonlee mew nidoran mewtwo golbat venusaur. Graveler mankey cloyster charmeleon seadra squirtle. Doduo butterfree venonat oddish dragonair bulbasaur pidgeot. Farfetchd charmeleon rattata poliwrath bulbasaur pidgeotto charizard. Dugtrio bulbasaur cloyster dugtrio meowth jolteon charizard.
+            
+            Bulbasaur clefairy vulpix jigglypuff meowth psyduck mew machop. Clefable kingler koffing vileplume nidorino venomoth. Ivysaur jolteon jolteon geodude vulpix charmeleon machop. Farfetchd jigglypuff wartortle venusaur mankey parasect. Psyduck cubone raticate cubone nidorino clefairy. Clefairy diglett persian clefable dragonite sandshrew venusaur. Clefable raticate ekans mew dugtrio venusaur nidoran.
+            
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in leo ultrices, ullamcorper elit in, tristique arcu. Fusce in finibus libero. Sed ac quam eu purus tristique venenatis. Vestibulum nec fringilla lectus, vel bibendum lacus. Quisque nec vulputate nisl. Suspendisse tincidunt lectus vel lectus lacinia, nec volutpat orci facilisis. Aenean sed justo in leo dignissim vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+    )
+     
 }
